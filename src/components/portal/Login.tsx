823d294e504989a3fc29dc1dd1e2ca59ef56d624
faker <skt.faker.ccj@gmail.com>
2020-12-16 20:49:49 +0800

    login

--- src/components/portal/Login.tsx
+++ src/components/portal/Login.tsx
@@ -1,6 +1,59 @@
 import * as React from 'react';
 import { PrimaryButton, Stack, Text, TextField } from '@fluentui/react';
 import AppContext from '../../AppContext';
+import axios from 'axios'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+
+const LoginUrl = "http://localhost:8080/user/login"
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080/user/',
+  responseType: 'json',
+  timeout: 50000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
+
+// const RequestType = '"get" | "GET" | "delete" | "DELETE" | "head" | "HEAD" | "options" | "OPTIONS" | "post" | "POST" | "put" | "PUT" | "patch" | "PATCH" | "purge" | "PURGE" | "link" | "LINK" | "unlink" | "UNLINK" | undefined'
+
+export function fetch(url:string, params:any,type:string) {
+  return new Promise((resolve, reject) => {
+      axiosInstance({
+        method: 'post',
+        url,
+        data:params,
+        responseType: type?'blob':'json',
+    }).then(res => {
+       resolve(res)
+    }).catch(err => {
+       reject(err)
+    })
+  })
+}
+
+
+const LoginPost = (name:string, password:string) =>{
+  console.log("username:%s",name);
+  console.log("password:%s",password);
+  // let data = {"username":name, "password":password};
+  let param = new URLSearchParams();
+  param.append("username",name);
+  param.append("password",password);
+  // axios.post(LoginUrl,data).then(
+  //   res=>{
+  //     console.log("res=>",res);
+  //   }
+  // ); 
+  axios({
+    method:"post",
+    url:LoginUrl,
+    data:param
+  });
+
+  // fetch("/login",data,"")
+  
+  
+};
 
 const Login: React.FunctionComponent = () => {
   const { setUser } = React.useContext(AppContext);
@@ -9,6 +62,7 @@ const Login: React.FunctionComponent = () => {
 
   const login = () => {
     setUser({ id: 1, name: name!, email: name! + "@test.com" })
+    LoginPost(name!,password!);
   };
 
   return <Stack>
