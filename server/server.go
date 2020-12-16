570b5b600e1de09a0c2a7a3ba131b4ff5496ed86
faker <skt.faker.ccj@gmail.com>
2020-12-16 12:51:02 +0800

    后端框架(粗糙)

--- server/server.go
+++ server/server.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	//设置分组路由
+	v1 := r.Group("/user")
+
+	//根据分组设置路由
+	{
+		v1.GET("/name", GetUserName)
+	}
+
+	//启动
 	r.Run() // listen and serve on 0.0.0.0:8080
+
+}
+
+func GetUserName(c *gin.Context) {
+	c.String(http.StatusOK, "Faker")
 }
