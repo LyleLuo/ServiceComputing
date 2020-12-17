61811ca2582fb4131453eed87e4017ac70f7b479
Steven He <hez2010@outlook.com>
2020-12-17 15:27:20 +0800

    feat: http options and headers

--- src/hooks/http.ts
+++ src/hooks/http.ts
@@ -1,9 +1,14 @@
-import useFetch from 'use-http';
+import useFetch, { Options } from 'use-http';
 
-export default function useHttp<T>(endpoint: string, method: 'POST' | 'PATCH' | 'PUT' | 'DELETE' | 'GET') {
-  const { post, put, patch, del, get, data, loading, error } = useFetch<T>(endpoint);
+export default function useHttp<T>(endpoint: string, method?: 'POST' | 'PATCH' | 'PUT' | 'DELETE' | 'GET', config?: (options: Options) => Options) {
+  const { post, put, patch, del, get, data, loading, error, response } = useFetch<T>(endpoint, options => {
+    if (config) {
+      config(options);
+    }
+    return options;
+  });
 
-  const fire = <R extends object>(data?: R): void => {
+  const fire = <R extends object>(data?: R, headers?: []): void => {
     if (method === 'POST') post(data);
     else if (method === 'PUT') put(data);
     else if (method === 'PATCH') patch(data);
@@ -16,5 +21,6 @@ export default function useHttp<T>(endpoint: string, method: 'POST' | 'PATCH' | 
     data,
     loading,
     error: error?.message,
+    headers: response.headers
   };
 }
