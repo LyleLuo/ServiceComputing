29f1810704ec25cb2fabd8dbaa59071a8433a2f2
freedomlp <1805244951@qq.com>
2020-12-21 15:48:12 +0800

    new Portal by lp

--- src/components/portal/Portal.tsx
+++ src/components/portal/Portal.tsx
@@ -1,17 +1,24 @@
-import { PrimaryButton } from "@fluentui/react";
+import { ActionButton, PrimaryButton, Stack } from "@fluentui/react";
 import * as React from "react";
 import AppContext from "../../AppContext";
+import { NavLink, useParams } from "react-router-dom";
 import useHttp from "../../hooks/http";
 import Login from "./Login";
+
+interface ArticleModel {
+  blog_id: string;
+  title: string;
+}
 
 const Portal: React.FunctionComponent = () => {
   const { user, setUser } = React.useContext(AppContext);
   const logoutRequest = useHttp<{ status: string }>("/api/user/logout", "POST");
+  const [list, setList] = React.useState<ArticleModel[]>();
 
   React.useEffect(() => {
     if (logoutRequest.data && !logoutRequest.loading) {
       if (logoutRequest.data.status === "success" && setUser) {
-        setUser(undefined);
+        setUser(undefined);  
       }
     }
   }, [logoutRequest.data, logoutRequest.loading]);
@@ -20,11 +27,53 @@ const Portal: React.FunctionComponent = () => {
     logoutRequest.fire();
   };
 
+  React.useEffect(() => {
+    fetch("/api/user/portal", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ author_id: user.id})
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw "failed to fetch"
+        } 
+        return res.json();
+      })
+      .then(data => {
+        if (data.status === "success") {
+          console.log(data)
+          setList(data.result);
+        } else {
+          alert("failed to load");
+        }
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }, []);
+
   return user ? <>
     <p>欢迎，{user.name}！</p>
     <p>邮箱：{user.email}</p>
     <p>Id：{user.id}</p>
+    <p>你所发布的全部博客：</p>
+    <Stack>
+      { 
+        list.map((v, i) => {
+          return <Stack.Item key={i} styles={{ root: { paddingTop: 10 } }}>
+              <p>Blog_id: {v.blog_id}</p>
+              <p>Aitle: {v.title}</p>
+              <PrimaryButton>
+                <NavLink style={{ textDecoration: "none", color: "white" }} to={`/details/${v.blog_id}`}>Go to details</NavLink>
+              </PrimaryButton>
+              <hr/>
+          </Stack.Item>;
+        })
+      }
+    </Stack>
     <PrimaryButton text="退出" onClick={logout} />
+
   </> : <Login />;
 };
 
